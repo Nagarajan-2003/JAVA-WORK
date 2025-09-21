986224623e03c957319e78657d121b386a4f2e01
Nagarajan M <nagarajanmnagarajan77@gmail.com>
2025-09-21 11:02:25 +0530

    Initail commit to compound interest calculator

---
+++ compound_interest_calculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class compound_interest_calculator{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double principal;
+        double rate;
+        int time;
+        int years;
+        double amount;
+        System.out.print("Enter the principal amount: ");
+        principal=sc.nextDouble();
+        System.out.print("Enter the interest rate (in %): ");
+        rate=sc.nextDouble();
+        System.out.print("Enter the time compounded per year: ");
+        time=sc.nextInt();
+        System.out.print("Enter the number of years: ");
+        years=sc.nextInt();
+        amount=principal*Math.pow(1+(rate/100)/time,time*years);
+        System.out.println("The amount after "+years+" years is: "+amount);
+        sc.close();
+    }
+}
